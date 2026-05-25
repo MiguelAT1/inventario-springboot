0554131f34e288ed1e0b2797c77cb30c8a999118
MINAYA GEORGE (APEEXT) <george.minaya@lindley.pe>
2026-05-25 12:20:16 -0500

    Retirada carpetas innecesarias

--- frontend-app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductosComponent } from './productos/productos.component';
-import { ListaProductosComponent } from './listaproductos/lista-productos.component';
-
-export const routes: Routes = [
-  { path: '', component: ListaProductosComponent },
-  { path: 'nuevo', component: ProductosComponent },
-  { path: 'nuevo/:id', component: ProductosComponent }
-];
